ada273be2cdb9082c3f8644d13d43d8ecd97ed99
Ranjith <ranjithp2294@gmail.com>
2024-06-28 14:37:12 +0530

    cookie setting has been updated for render

--- src/controllers/middleware/tokenOptions.ts
+++ src/controllers/middleware/tokenOptions.ts
@@ -12,31 +12,72 @@ interface ITokenOptions {
   maxAge: number;
   httpOnly: boolean;
   sameSite: "lax" | "strict" | "none" | undefined;
-  domain?:string;
-  path?:string;
+  domain?: string;
+  path?: string;
   secure?: boolean;
 }
 
 const accessTokenProductionMode =
   process.env.NODE_ENV === "production" ? true : false;
 
-
 // options for cookies
 export const accessTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000), //5 hour
   maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "strict",
-  // domain: '.digi-world.online',
-  // path: '/',
+  sameSite: "none",
+  domain: '.digi-world.online',
+  path: '/',
   secure: accessTokenProductionMode,
 };
 export const refreshTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000), // 3 days
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "strict",
-  // domain: '.digi-world.online',
-  // path: '/',
+  sameSite: "none",
+  domain: '.digi-world.online',
+  path: '/',
   secure: accessTokenProductionMode,
 };
+
+// const accessTokenExpire = parseInt(
+//   process.env.ACCESS_TOKEN_EXPIRE || "300",
+//   10
+// );
+// const refreshTokenExpire = parseInt(
+//   process.env.REFRESH_TOKEN_EXPIRE || "1200",
+//   10
+// );
+
+// interface ITokenOptions {
+//   expires: Date;
+//   maxAge: number;
+//   httpOnly: boolean;
+//   sameSite: "lax" | "strict" | "none" | undefined;
+//   domain?:string;
+//   path?:string;
+//   secure?: boolean;
+// }
+
+// const accessTokenProductionMode =
+//   process.env.NODE_ENV === "production" ? true : false;
+
+// // options for cookies
+// export const accessTokenOptions: ITokenOptions = {
+//   expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000), //5 hour
+//   maxAge: accessTokenExpire * 60 * 60 * 1000,
+//   httpOnly: true,
+//   sameSite: "strict",
+//   // domain: '.digi-world.online',
+//   // path: '/',
+//   secure: accessTokenProductionMode,
+// };
+// export const refreshTokenOptions: ITokenOptions = {
+//   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000), // 3 days
+//   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+//   httpOnly: true,
+//   sameSite: "strict",
+//   // domain: '.digi-world.online',
+//   // path: '/',
+//   secure: accessTokenProductionMode,
+// };
